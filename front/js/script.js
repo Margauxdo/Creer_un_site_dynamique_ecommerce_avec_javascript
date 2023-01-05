30643d263587d65b52770b7278e8bd9f1f91be98
margaux <margauxdoisy@gmail.com>
2023-01-05 11:43:28 +0100

    commit -m "ajout du server et fichier js dans le front"

---
+++ front/js/script.js
@@ -0,0 +1,30 @@
+fetch('http://localhost:3000/api/products');
+
+const obj={
+    "name": "kanap-backend",
+    "version": "1.0.0",
+    "description": "",
+    "main": "server.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+      "express": "^4.16.4",
+      "uuid": "^3.3.2"
+    }
+  }
+
+  
+fetch('http://localhost:3000/api/products') 
+.then(function(response){
+    if(response.ok){
+        return response.json();
+    }
+})
+.then(function(value){
+    console.log(value);
+})
+.catch(function(err){
+});
