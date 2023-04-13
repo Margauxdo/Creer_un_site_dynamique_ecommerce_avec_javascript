c3fb9405ccc79fbe31824ea2ea304c8c075aedaa
margaux <margauxdoisy@gmail.com>
2023-04-13 15:12:50 +0200

    commit -M "ajout de image, titre et description"

--- front/js/script.js
+++ front/js/script.js
@@ -12,12 +12,28 @@ fetchAPI();
  function showProducts(data) {
   for (products of data){
 
-    const lienElement = document.createElement("a");
+    const lienElement = document.createElement("a"); //**ajout du lien qui englobe les infos */
     lienElement.href = "./product.html?id=" + products._id; //* url du lien du produit + _id pcq id varie *//
-    const appendArticle = document.getElementById('items').appendChild(lienElement);
+    const appendArticle = document.getElementById('items').appendChild(lienElement);/*items est enfant du lien a */
     console.log(products._id);
 
-    const articleElement = document.createElement("article");
+    const articleElement = document.createElement("article"); /*cree article */
     appendArticle.appendChild(articleElement);
+
+    const imageElement = document.createElement("img"); /* creation de image */
+    imageElement.src = products.imageUrl;
+    appendArticle.appendChild(imageElement);
+
+    const productNameElement = document.createElement("h3"); /* creation du titre */
+    productNameElement.innerText = products.name;
+    appendArticle.appendChild(productNameElement);
+
+    const productDescriptionElement = document.createElement("p");/*creation de la description*/
+    productDescriptionElement.innerText = products.description;
+    appendArticle.appendChild(productDescriptionElement);
+
+
+
+    
   }
  }
