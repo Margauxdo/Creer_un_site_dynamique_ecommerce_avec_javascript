3adc2530d1d1ff30bc37d105ee16e0a309f954f6
margaux <margauxdoisy@gmail.com>
2023-04-13 16:22:41 +0200

    commit-M""

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-//* fonction fetchAPI avec fetch et uen promise then *//
+//* fonction fetchAPI avec fetch et uen promise then  -etape 2-*//
 function fetchAPI() {
 fetch("http://localhost:3000/api/products")
 .then ((response) => response.json())
@@ -8,7 +8,7 @@ fetch("http://localhost:3000/api/products")
 }
 
 fetchAPI();
- //*fonction qui relie les produits au data*//
+ //*fonction qui relie les produits au data -etape 3-*//
  function showProducts(data) {
   for (products of data){
 
@@ -25,6 +25,7 @@ fetchAPI();
     imageElement.width="300";
     imageElement.height = "200";
     appendArticle.appendChild(imageElement);
+    console.log(products.imageUrl);
 
     const productNameElement = document.createElement("h3"); /* creation du titre */
     productNameElement.innerText = products.name;
