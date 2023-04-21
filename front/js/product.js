d9f47157ae955dcb09b32c977a7d69436204c1ac
margaux <margauxdoisy@gmail.com>
2023-04-21 11:28:18 +0200

    commit -M " ajout des fonction"

--- front/js/product.js
+++ front/js/product.js
@@ -11,30 +11,55 @@ const fetchArticle = () => {     //**variable pour recuperer un seul produit en 
      return response.json()   //on fait un retour a la reponse en json//
     })  // on utilise une deuxieme fois then pour acceder au data de api //
     .then(function(data)  {
-
+      titleKanap(data)
+      priceKanap(data)
+      descriptionKanap(data)
+      optionColorskanap(data)
+      ToCartKanap(data)
+      localStorageToCart(data)
+      
       const appendProduct = document.getElementsByClassName('item') //on cree une variable afin de recuperer emplacement des elements//
       
-      const articleTitle = (document.getElementById("title").innerText = data.name) //on cree une variable pour recuperer le titre qui est appele title en html et data.name, dans le date il est appele name//
-      const articlePrice = (document.getElementById("price").innerText = data.price)  //on cree une variable pour recup le prix//
+      function titleKanap(){
+        const articleTitle = (document.getElementById("title").innerText = data.name) //on cree une variable pour recuperer le titre qui est appele title en html et data.name, dans le date il est appele name//
+      }
+
+      function priceKanap(){
+        const articlePrice = (document.getElementById("price").innerText = data.price)  //on cree une variable pour recup le prix//
+      }
+      
       //on doit crée la balise de image qui n'existe pas//
-      const articleImg = document.createElement("img")
-      articleImg.src = data.imageUrl
-      //
-      //console.log(articleImg)
+      
+      function imageKanap(imageUrl,altTxt){
+        const articleImg = document.createElement("img");
+      //articleImg.src = data.imageUrl
+        articleImg.setAttribute('src',imageUrl);
+        articleImg.setAttribute('alt', altTxt);
+        articleImg.width = '200';
+        articleImg.height = '150';
+        document.body.appendChild(articleImg);
+        console.log(articleImg);
+    }
       //on selection avec queryselector ou image doit est positionné en fonction prodcut;html, qui est enfant de la const image articleImg//
       //document.querySelector(".item_img").appendProduct(articleImg)
       //ajoute url de image //
       //articleImg.setAttribute("src",`$'{data.imageUrl}`)
       
+    function descriptionKanap() {
       const articleDescription = (document.getElementById("description").innerText = data.description) //on cree une variable pour recuperer la description//
-    
+    }
+
+    function optionColorskanap() {
       //on va cree une variable avec les differentes options de couleurs//
       const articleOptionsColors = document.getElementById("colors")
-      for (color in data.colors){ //on utilise la boucle for in pour recuperer les couleurs dans le data //
+        for (color in data.colors){ //on utilise la boucle for in pour recuperer les couleurs dans le data //
         //je reprend la constance articleOptionsColors ou je lui ajoute du html , on reprend le code html ou on y integre une possibilité de couleurs dans les couleurs du data//
-        articleOptionsColors.innerHTML += `<option value = "${data.colors[color]}">${data.colors[color]}</option>`
+          articleOptionsColors.innerHTML += `<option value = "${data.colors[color]}">${data.colors[color]}</option>`
       }
+    } 
 
+
+    function ToCartKanap (){
       //on créé une variable pour acceder au bouton du panier // 
       const addToCart = document.getElementById("addToCart")
       //des on clique sur le bouton ca enregistre les evenment//
@@ -46,26 +71,24 @@ const fetchArticle = () => {     //**variable pour recuperer un seul produit en 
           
         }
         //console.log(productsToCart)
-
-
-
       })
-      
-    
+    }
     })
+  
 
 }
 
+function localStorageToCart(){
 //on sort de la fonction pour ajouter tout ca au localstorage//
-localStorageProductsToCart = []
+  localStorageProductsToCart = []
 //pour recup dans le localstorage si item addtocart est pas nul on peut recuperer//
-if(localStorage.getItem('addTocart')) {
-  localStorageProductsToCart()
+  if(localStorage.getItem('addTocart')) {
+    localStorageProductsToCart()
+  }
+  else{
+
+  } 
 }
-else{
-
-} 
-
 
 
 
