9ecd77beb78f6b5e6fff53336f6ec0245ff08362
margaux <margauxdoisy@gmail.com>
2023-05-09 17:35:02 +0200

    commit -M "ajout dans la page panier id, couleur, quantite et prix"

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,40 +1,86 @@
  const productsLocalStorage = JSON.parse(localStorage.getItem("addToCart"));
 //Je recupere le donnees stocké dans le localStoarge avec la cle addToCart  et utilise la methode JSOn.parse , je socke les produits recement ajoutés au panier et je peut les recup dans la page panier//
 
-     const cartProduct = document.getElementsByClassName('cart') ;
+     const cartProduct = document.querySelector(".cart");
      console.log(cartProduct);
 //je cree une variable pour preciser que les element se situeront dans la class "cart" dans le html
 
-if (productsLocalStorage === null || productsLocalStorage.length === 0) {
+if (!productsLocalStorage || productsLocalStorage.length === 0) {
     console.log("mon panier est vide");//si le localstorage est null ce qui equivaut a 0 alors le panier est vide//
-}else {
+const infoUsers = document.createElement("p");
+infoUsers.textContent = "Votre  panier est vide";
+cartProduct.appendChild(infoUsers);
+//message pour informer utilisateur que le panier est vide//
+  }else {
     console.log("mon panier n'est pas vide!!");//sinon mon panier n'est pas vide//
-
-     
 //je cree une fonction en utilisant foreach pour parcourir le tableau du localstorage et recupéré id, la couleur et la quantité//
-productsLocalStorage.forEach(cartProduct => {
-  console.log(`Produit ${cartProduct.id}  Couleur ${cartProduct.colors}  Quantité ${cartProduct.quantity}`);
-//je recupere dans le locale storage id que je vais nommé produit, la couelur et la quantité qui sera placé grace a la variable cartProducts//
-console.log(productsLocalStorage);
-
-//j'utilise map afin de recuperer id des produits dans api dans un ordre specifique + Promise.all afin effectuer les requêtes en paralleles//
-  function cartFromApi() {
-    Promise.all(
-        productsLocalStorage.map((id) => {
-      return fetch(`http://localhost:3000/api/products/${id}`)
-          .then ((response) => response.json())
+productsLocalStorage.forEach(product => {
+  //recup les infos depuis API//
+fetch(`http://localhost:3000/api/products/${product.id}`)   
+      .then ((response) => response.json())
           .then((data) => {
-             console.log(data);
-    })
+  //cree une div pour afficher le produit//
+            const productDiv = document.createElement('article');
+            productDiv.classList.add('cart__item');
+            console.log(productDiv);
+  //afficher le nom et le prix du produit//
+            const productName = document.createElement('h2');
+            productName.textContent = data.name;
+            productDiv.appendChild(productName);
+            console.log(productName);
+
+            const productPrice = document.createElement('p');
+            productPrice.textContent = `${data.price} € `;
+            productDiv.appendChild(productPrice);
+            console.log(productPrice);
+
+  //afficher la quantité, la couleur de produit depuis le localstorage
+            const productQuantity = document.createElement('p');
+            productQuantity.textContent = `Quantité : ${product.quantity}`;
+            productDiv.appendChild(productQuantity);
+            console.log(productQuantity);
+
+            const productColor = document.createElement('p');
+            productColor.textContent = `Couleur : ${product.color}`;
+            productDiv.appendChild(productColor);
+            console.log(productColor);
+
+//  Ajouter le produit a la page panier//
+            cartProduct.appendChild(productDiv);
           }
-    ))
-      } 
+          )
+        }
+        )
+//******MODIFIER LA COULEUR POU LA DEFINIR EN ASSOCIANT A API ET AJOUTER LA PHOTO ET AFFICHER */
+
+            //productDiv.innerHTML = cartProduct;
+            
+          }
+    
+  
+  
+    //function imageShow(data){
+    //const productImage = document.createElement("img");
+    //productImage.src = data.imageUrl;/*on relie la source de image */
+    //const cartItemImage = document.querySelector(".cart__item__img");
+    //cartItemImage.appendChild(productImage);
+    //console.log(cartItemImage);
+   //}
+
+    
+
+      
+
+
+    /*on modifie la hauteur de image*/
+ 
+
+
+    //articleElement.appendChild(imageElement);/*image est enfant de article*/
+    //onsole.log(products.imageUrl);
     //ajout des paniers de api //
     //image//prix
-  });
-    }
-    ;
-
+  
 
 
 
